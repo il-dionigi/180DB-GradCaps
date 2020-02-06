a5abe26cb385a4e6bab5b1957586ae6c27c6fc73
Charles Zaloom <czaloom4@gmail.com>
2020-02-06 14:23:11 -0800

    Update strandtest.ino

--- Display/strandtest/strandtest.ino
+++ Display/strandtest/strandtest.ino
@@ -18,6 +18,12 @@ https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
 http://arduino.esp8266.com/stable/package_esp8266com_index.json
 https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 
+Now goto Library Manager and search/install:
+ - Adafruit mqtt
+ - Adafruit neopixel
+
+
+
 */
 
 #include <Adafruit_NeoPixel.h>
