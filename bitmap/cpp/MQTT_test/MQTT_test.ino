79466ca274981a499a985b92871fdcbcfa99132a
il-dionigi <brian.dionigi@gmail.com>
2020-02-13 10:45:21 -0800

    horizontal initial working
    not tested, but doesn't crash and displays

--- bitmap/cpp/MQTT_test/MQTT_test.ino
+++ bitmap/cpp/MQTT_test/MQTT_test.ino
@@ -35,7 +35,7 @@ Adafruit_MQTT_Subscribe test = Adafruit_MQTT_Subscribe(&mqtt, "test_channel");
 void MQTT_connect();
 
 // Create a pointer to a bitmap class
-bitmap bm = bitmap(5,3,&strip);
+bitmap bm = bitmap(5,4,&strip);
 
 void setup() {
   Serial.begin(115200);
@@ -45,7 +45,8 @@ void setup() {
   strip.setBrightness(255);
 
   bm.generate_msg_v("UCLA!");
-  bm.generate_sequence_v(5, 5, 0);
+  bm.generate_sequence_v(5, 0, 0);
+  bm.show_sequence_delay(1000,strip.Color(200,200,0));
 
   Serial.println(F("Adafruit MQTT demo"));
 
@@ -92,7 +93,7 @@ void loop() {
         colorWipe(strip.Color(0,0,255));
       }
       else if(test.lastread[0] == 'C' && test.lastread[1] == '1'){
-        bm.show_sequence_v2(1000, strip.Color(0,0,255));
+        bm.show_sequence_delay(1000, strip.Color(0,0,255));
       }
     }
   }
--- bitmap/cpp/MQTT_test/bitmap.h
+++ bitmap/cpp/MQTT_test/bitmap.h
@@ -13,6 +13,7 @@ private:
   bool* m_seq_v;
   int m_msg_len;
   int m_seq_len;
+  int m_row_len;
   int m_rows;
   int m_columns;
   ORIENTATION m_orientation;
@@ -27,7 +28,9 @@ public:
   void print_char(char c);
   bool generate_msg_v(char* msg);
   bool generate_sequence_v(int iterations, int x, int y);
-  void show_sequence(long interval_ms, uint32_t color);
+  void show_sequence_delay(long interval_ms, uint32_t color);
+  void show_sequence_nodelay(long interval_ms, uint32_t color);
   // can't think of good name, like the OG but lights up by column during interval
-  void show_sequence_v2(long interval_ms, uint32_t color);
+  void show_sequence_scroll_delay(long interval_ms, uint32_t color);
+  void show_sequence_scroll_nodelay(long interval_ms, uint32_t color);
 };
