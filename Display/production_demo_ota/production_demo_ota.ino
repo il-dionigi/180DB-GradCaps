4503008f27749e2fefe335b5f590afb29a5e3206
Charles Zaloom <czaloom4@gmail.com>
2020-02-24 16:27:19 -0800

    update

--- Display/production_demo_ota/production_demo_ota.ino
+++ Display/production_demo_ota/production_demo_ota.ino
@@ -86,6 +86,9 @@ void setup() {
 
   //--OTA--
 
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(String(MY_ROLE));  
+
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
   });
